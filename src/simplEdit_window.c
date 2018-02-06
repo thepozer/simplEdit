b3dd1e0338edc03c0469ab0a8ac7d6c17e6e7f08
Prolhac Didier <didier.prolhac-ext@amundi.com>
2018-02-06 18:00:05 +0100

    Add extra wdiget to save as dialog

--- src/simplEdit_window.c
+++ src/simplEdit_window.c
@@ -6,6 +6,7 @@ GtkWidget * pWndEdit = NULL;
 SEditorData gblData = {NULL, NULL, NULL, NULL, NULL, NULL, NULL, TRUE} ;
 
 void simplEdit_window_updateTitle(SEditorData * pEditData);
+GtkWidget * simplEdit_window_extraWidget(SEditorData * pEditData);
 
 void simplEdit_window_init(GtkBuilder * pBuilder) {
 	simplEdit_content_init(&gblData, pBuilder);
@@ -87,6 +88,66 @@ void smpldt_clbk_menu_file_save (GtkMenuItem *menuitem, gpointer user_data) {
 	}
 }
 
+GtkWidget * simplEdit_window_extraWidget(SEditorData * pEditData) {
+	
+	GtkWidget * pHBox, * pLabel, * pLstEncoding, * pLstEndOfLines, * pLstCompress;
+	GtkListStore * pLstModelEOL;
+	GtkTreeIter iter, * pIterSelEOL;
+	GtkSourceNewlineType typeEOL;
+	
+	pHBox = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 2);
+	gtk_widget_show(pHBox);
+	
+	pLabel = gtk_label_new("Encoding :");
+	gtk_widget_show(pLabel);
+	gtk_box_pack_start(GTK_BOX(pHBox), pLabel, TRUE, TRUE, 1);
+
+	pLstEncoding = gtk_combo_box_text_new();
+	gtk_combo_box_text_append(GTK_COMBO_BOX_TEXT(pLstEncoding), "utf-8", "UTF-8");
+	gtk_combo_box_text_append(GTK_COMBO_BOX_TEXT(pLstEncoding), "iso8859-1", "ISO8859-1 - latin1");
+	gtk_combo_box_text_append(GTK_COMBO_BOX_TEXT(pLstEncoding), "iso8859-15", "ISO8859-15 - Latin9");
+	gtk_widget_show(pLstEncoding);
+	gtk_box_pack_start(GTK_BOX(pHBox), pLstEncoding, TRUE, TRUE, 1);
+
+	pLabel = gtk_label_new("End of line :");
+	gtk_widget_show(pLabel);
+	gtk_box_pack_start(GTK_BOX(pHBox), pLabel, TRUE, TRUE, 1);
+	
+	typeEOL = gtk_source_file_get_newline_type(pEditData->pSrcFile);
+	pLstModelEOL = gtk_list_store_new (2, G_TYPE_INT, G_TYPE_STRING);
+	gtk_list_store_append (pLstModelEOL, &iter);
+	gtk_list_store_set (pLstModelEOL, &iter, G_TYPE_INT, GTK_SOURCE_NEWLINE_TYPE_LF,   G_TYPE_STRING, "Unix (\\n)", -1);
+	if (typeEOL == GTK_SOURCE_NEWLINE_TYPE_LF) {
+		pIterSelEOL = gtk_tree_iter_copy(&iter);
+	}
+	gtk_list_store_append (pLstModelEOL, &iter);
+	gtk_list_store_set (pLstModelEOL, &iter, G_TYPE_INT, GTK_SOURCE_NEWLINE_TYPE_CR_LF, G_TYPE_STRING, "Windows (\\r\\n)", -1);
+	if (typeEOL == GTK_SOURCE_NEWLINE_TYPE_LF) {
+		pIterSelEOL = gtk_tree_iter_copy(&iter);
+	}
+	gtk_list_store_append (pLstModelEOL, &iter);
+	gtk_list_store_set (pLstModelEOL, &iter, G_TYPE_INT, GTK_SOURCE_NEWLINE_TYPE_CR,   G_TYPE_STRING, "Mac (\\r)", -1);
+	if (typeEOL == GTK_SOURCE_NEWLINE_TYPE_LF) {
+		pIterSelEOL = gtk_tree_iter_copy(&iter);
+	}
+	pLstEndOfLines = gtk_combo_box_new_with_model(GTK_TREE_MODEL(pLstModelEOL));
+	gtk_combo_box_set_active_iter(GTK_COMBO_BOX(pLstModelEOL), pIterSelEOL);
+	gtk_widget_show(pLstEndOfLines);
+	gtk_box_pack_start(GTK_BOX(pHBox), pLstEndOfLines, TRUE, TRUE, 1);
+
+	pLabel = gtk_label_new("Compression :");
+	gtk_widget_show(pLabel);
+	gtk_box_pack_start(GTK_BOX(pHBox), pLabel, TRUE, TRUE, 1);
+
+	pLstCompress = gtk_combo_box_text_new();
+	gtk_combo_box_text_append(GTK_COMBO_BOX_TEXT(pLstCompress), "none", "None");
+	gtk_combo_box_text_append(GTK_COMBO_BOX_TEXT(pLstCompress), "gzip", "Gzip (.gz)");
+	gtk_widget_show(pLstCompress);
+	gtk_box_pack_start(GTK_BOX(pHBox), pLstCompress, TRUE, TRUE, 1);
+	
+	return pHBox;
+}
+
 void smpldt_clbk_menu_file_saveas (GtkMenuItem *menuitem, gpointer user_data) {
 	GtkWidget * pDlgFile;
 	gint iResult;
@@ -103,6 +164,10 @@ void smpldt_clbk_menu_file_saveas (GtkMenuItem *menuitem, gpointer user_data) {
 	} else {
 		gtk_file_chooser_set_current_name(GTK_FILE_CHOOSER(pDlgFile), "New file");
 	}
+	
+	GtkWidget * pHBox = simplEdit_window_extraWidget(&gblData);
+	
+	gtk_file_chooser_set_extra_widget(GTK_FILE_CHOOSER(pDlgFile), pHBox);
 	
 	iResult = gtk_dialog_run (GTK_DIALOG (pDlgFile));
 //g_print("smpldt_clbk_menu_file_saveas - iResult : %i\n", iResult);
